8739d37434c5446ae0e9e4c70940d78c777169a6
asmahrichelle9 <asmahrichelle9@gmail.com>
2026-04-06 21:31:30 +0000

    feat: create Preview page

---
+++ src/pages/Preview.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChatPreview from '../components/ChatPreview';
+
+const Preview = ({ nodes }) => {
+  return (
+    <ChatPreview nodes={nodes} />
+  );
+};
+
+export default Preview;
